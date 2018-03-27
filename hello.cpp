2f1fff12031ee2ac180563f8c4c0d3421b17edf6
lym0302 <lym0302@foxmail.com>
2018-03-27 04:03:31 +0000

    hello

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+/* hello.cpp */
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	// You also can use "\n" to replace endl.
+	cout << "hello" << "\n";
+	// You can use "std::cout" when "using namespace std;" is not in the program.
+	std::cout << "Hi!" << std::endl;
+	return 0;
+}
+
+
+
